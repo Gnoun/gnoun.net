f101d42d89f8df4639b78c8872fcc72914fa971b
Gnoun <Gnoun@users.noreply.github.com>
2021-10-15 18:57:45 +0000

    deploy: dc659dc6d8fbfbce4536cc0b9ad9c6452acd409c

--- _nuxt/static/1633782048/pl/math/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1633782048",layout:"default",error:a,state:{pageTitle:"Math Tools",currentTab:"math",i18n:{routeParams:{}}},serverRendered:true,routePath:"\u002Fpl\u002Fmath",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"fb972740"}}}}(null));
---
+++ _nuxt/static/1634324250/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1634324250",layout:"default",error:a,state:{pageTitle:"Home",currentTab:"home",i18n:{routeParams:{}}},serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"2ac20776"}}}}(null,"\u002F"));
