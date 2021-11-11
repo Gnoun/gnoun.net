c4c625683aba8268a8afe6b8b9eaef5d55e31f91
Gnoun <Gnoun@users.noreply.github.com>
2021-11-11 13:00:47 +0000

    deploy: 0a4579741a2a6281384451496d15675df409956e

---
+++ _nuxt/static/1636635632/other/other/other/58/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1636635632",layout:"default",error:a,state:{pageTitle:"???",currentTab:"other",i18n:{routeParams:{}}},serverRendered:true,routePath:"\u002Fother\u002Fother\u002Fother\u002F58",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b348f435"}}}}(null));
